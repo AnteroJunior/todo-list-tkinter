3b3b12fa3bc7be33c9fa70a829232a665325c9b1
Antero Júnior <anterojunior1530@gmail.com>
2024-06-05 23:53:22 -0300

    fix: Adding integrity check

--- Models/Database.py
+++ Models/Database.py
@@ -9,9 +9,9 @@ class Database:
         self.connection = Connection()
     
     def create_tables(self):
-        self.connection.cursor.execute(""" CREATE TABLE IF NOT EXISTS users (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, userName VARCHAR(100) NOT NULL, userEmail VARCHAR(100) NOT NULL UNIQUE, userPassword VARCHAR(100) NOT NULL)""")
+        self.connection.cursor.execute(""" CREATE TABLE IF NOT EXISTS users (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, userName VARCHAR(100) NOT NULL, userEmail VARCHAR(100) NOT NULL UNIQUE, userPassword VARCHAR(100) NOT NULL, CHECK(userName <> ''), CHECK(userEmail <> ''))""")
 
-        self.connection.cursor.execute(""" CREATE TABLE IF NOT EXISTS tasks (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, taskDesc VARCHAR(100) NOT NULL, taskStatus INTEGER(1) NOT NULL DEFAULT 0, idUser INTEGER NOT NULL REFERENCES users(id))""")
+        self.connection.cursor.execute(""" CREATE TABLE IF NOT EXISTS tasks (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, taskDesc VARCHAR(100) NOT NULL, taskStatus INTEGER(1) NOT NULL DEFAULT 0, idUser INTEGER NOT NULL REFERENCES users(id), CHECK(taskDesc <> ''), CHECK(taskStatus IN (0, 1)))""")
 
     def check_user(self, email):
         self.connection.cursor.execute(f'SELECT * FROM users WHERE userEmail = ?', (email,))
